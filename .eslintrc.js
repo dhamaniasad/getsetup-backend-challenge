d707f54da22990fad3d91cd00beb85a3accdd219
Asad Dhamani <dhamaniasad@gmail.com>
2021-12-29 16:02:26 +0530

    Add eslint
    Signed-off-by: Asad Dhamani <dhamaniasad@gmail.com>

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb-base',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    "no-console": "off"
+  },
+};
--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+  res.send('Hello, World!');
 });
 
 app.listen(PORT, () => {
-    console.log(`The application is running on port ${PORT}`);
+  console.log(`The application is running on port ${PORT}`);
 });

e66473bcce0de724eb019cea25b4b919edcaa006
Asad Dhamani <dhamaniasad@gmail.com>
2021-12-29 16:10:24 +0530

    Add type annotations to index.ts
    Signed-off-by: Asad Dhamani <dhamaniasad@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import express from 'express';
+import express, { Express, Request, Response } from 'express';
 
-const app = express();
+const app: Express = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello, World!');
 });
 

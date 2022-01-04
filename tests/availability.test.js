af2a695d29fff897b65a33c7acc6a6a7cbf3995a
Asad Dhamani <dhamaniasad@gmail.com>
2022-01-04 16:01:41 +0530

    Organize tests
    Signed-off-by: Asad Dhamani <dhamaniasad@gmail.com>

--- tests/availability.test.js
+++ tests/availability.test.js
@@ -9,68 +9,69 @@ beforeAll(async () => await db.connect());
 afterEach(async () => await db.clearDatabase());
 afterAll(async () => await db.closeDatabase());
 
-test("GET /api/availability/user/<userId>", async () => {
-   const user = await User.create({
-      email: "test@example.com",
-      password: "password"
-   })
-   const availability = await Availability.create({
-      year: 2022,
-      week: 1,
-      userId: user._id,
-      availability: [[], [], [], [], [], [], []]
-   });
+describe("GET /api/availability/user/<userId>", () => {
+    it("Returns correct availability for user", async () => {
+        const user = await User.create({
+            email: "test@example.com",
+            password: "password"
+        })
+        const availability = await Availability.create({
+            year: 2022,
+            week: 1,
+            userId: user._id,
+            availability: [[], [], [], [], [], [], []]
+        });
 
-   await supertest(app).get(`/api/availability/user/${user._id}`)
-       .expect(200)
-       .then(response => {
-          expect(response.body.status).toEqual(0);
-          expect(response.body.data.length).toBe(1);
-          expect(response.body.data[0].availability.length).toBe(7);
-          expect(response.body.data[0].availability).toEqual([
-              [],
-              [],
-              [],
-              [],
-              [],
-              [],
-              []
-          ]);
-       })
+        await supertest(app).get(`/api/availability/user/${user._id}`)
+            .expect(200)
+            .then(response => {
+                expect(response.body.status).toEqual(0);
+                expect(response.body.data.length).toBe(1);
+                expect(response.body.data[0].availability.length).toBe(7);
+                expect(response.body.data[0].availability).toEqual([
+                    [],
+                    [],
+                    [],
+                    [],
+                    [],
+                    [],
+                    []
+                ]);
+            })
+    });
 });
 
+describe("PUT /api/availability/user/<userId>", () => {
+    it("Stores availability data correctly via API", async () => {
+        const user = await User.create({
+            email: "test@example.com",
+            password: "password"
+        });
 
-test("PUT /api/availability/user/<userId>", async () => {
-   const user = await User.create({
-      email: "test@example.com",
-      password: "password"
-   });
+        const testData = {
+            "year": 2021,
+            "week": 3,
+            "availability": [
+                [],
+                ["08:00", "09:00", "10:00", "13:00", "15:00", "18:00"],
+                [],
+                [],
+                [],
+                ["08:00", "09:00", "10:00", "13:00", "15:00", "18:00"],
+                []
+            ]
+        };
 
-   const testData = {
-      "year": 2021,
-      "week": 3,
-      "availability": [
-         [],
-         ["08:00", "09:00", "10:00", "13:00", "15:00", "18:00"],
-         [],
-         [],
-         [],
-         ["08:00", "09:00", "10:00", "13:00", "15:00", "18:00"],
-         []
-      ]
-   };
+        await supertest(app).put(`/api/availability/user/${user._id}`)
+            .send(testData)
+            .expect(200)
+            .then(response => {
+                expect(response.body.status).toEqual(0);
+                expect(response.body.data).toMatchObject(testData);
+            })
+    });
 
-   await supertest(app).put(`/api/availability/user/${user._id}`)
-       .send(testData)
-       .expect(200)
-       .then(response => {
-          expect(response.body.status).toEqual(0);
-          expect(response.body.data).toMatchObject(testData);
-       })
-});
-
-test("PUT /api/availability/user/<userId>", async () => {
-    it("Blocks submissions before 7AM and after 10PM", async (done) => {
+    it("Blocks submissions before 7AM and after 10PM", async () => {
         MockDate.set(new Date("1/1/2022 06:00:00"));
 
         const user = await User.create({

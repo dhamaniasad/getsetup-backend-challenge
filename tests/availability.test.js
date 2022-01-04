b4d577a2f704d84d0f609d32fb92267faf29b3a8
Asad Dhamani <dhamaniasad@gmail.com>
2022-01-04 15:56:56 +0530

    WIP tests
    Signed-off-by: Asad Dhamani <dhamaniasad@gmail.com>

--- tests/availability.test.js
+++ tests/availability.test.js
@@ -3,6 +3,7 @@ const db = require("./db");
 const supertest = require("supertest");
 const Availability = require("../dist/src/models/Availability").default;
 const User = require("../dist/src/models/User").default;
+const MockDate = require("mockdate");
 
 beforeAll(async () => await db.connect());
 afterEach(async () => await db.clearDatabase());
@@ -68,3 +69,36 @@ test("PUT /api/availability/user/<userId>", async () => {
        })
 });
 
+test("PUT /api/availability/user/<userId>", async () => {
+    it("Blocks submissions before 7AM and after 10PM", async (done) => {
+        MockDate.set(new Date("1/1/2022 06:00:00"));
+
+        const user = await User.create({
+            email: "test@example.com",
+            password: "password"
+        });
+
+        const testData = {
+            "year": 2021,
+            "week": 3,
+            "availability": [
+                [],
+                ["08:00", "09:00", "10:00", "13:00", "15:00", "18:00"],
+                [],
+                [],
+                [],
+                ["08:00", "09:00", "10:00", "13:00", "15:00", "18:00"],
+                []
+            ]
+        };
+
+        await supertest(app).put(`/api/availability/user/${user._id}`)
+            .send(testData)
+            .expect(200)
+            .then(response => {
+                expect(response.body.status).toEqual(-1);
+                expect(response.body.message).toEqual("Availability can only be submitted between 7AM to 10PM. Please try again later.");
+                MockDate.reset();
+            })
+    })
+});

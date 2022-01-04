55d828ceb33adc87304363b69432f21b81be7fcb
Asad Dhamani <dhamaniasad@gmail.com>
2022-01-04 14:18:32 +0530

    Add jest
    Signed-off-by: Asad Dhamani <dhamaniasad@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -22,3 +22,14 @@ app.use('/api/user', user);
 app.listen(PORT, () => {
   console.log(`The application is running on port ${PORT}`);
 });
+
+// To make sure the PORT is released when Nodemon restarts the server
+// https://stackoverflow.com/questions/61181302/nodemon-error-listen-eaddrinuse-address-already-in-use-5000
+process.once('SIGUSR2', function () {
+  process.kill(process.pid, 'SIGUSR2');
+});
+
+process.on('SIGINT', function () {
+  // this is only called on ctrl+c, not restart
+  process.kill(process.pid, 'SIGINT');
+});
